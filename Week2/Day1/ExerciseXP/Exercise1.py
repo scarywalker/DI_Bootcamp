7a68ca2d0aa61ee3bb8ec7fd5b0e7b33a8c9e42c
Gabriel Dischon <gabrielgdischon@gmail.com>
2023-07-16 14:37:30 +0300

    Finnishedweek1day1exercisexp

---
+++ Week2/Day1/ExerciseXP/Exercise1.py
@@ -0,0 +1,46 @@
+# exercise 1
+# print("Hello world\nHello world\nHello world\nHello world")
+
+# exercise 2
+# print((99**3)*8)
+
+# exercise 3
+# False, True, False, Error, False
+
+# exercise 4
+# computer_brand = "Lenovo"
+# print("I have a "+computer_brand+" computer")
+
+# exercise 5
+# name="Gabriel"
+# age="26"
+# shoe_size="40"
+# info="I am "+name+", "+age+" years old, my shoe size is "+shoe_size+" and I am boring"
+# print(info)
+
+# exercise 6
+# a = 2
+# b = 1
+# if a > b:
+#     print("Hello World")
+
+# # exercise 7
+# # number = input("gime a number: ")
+# if (int(number)*3)%2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+
+# # exercise 8
+# # name=input("what's your name: ")
+# if name.lower() == "gabriel" :
+#     print("funny message we have the same name")
+# else:
+#     print("funny message we don't have the same name")
+
+# # exercise 9
+height=input("what's your height in dumb units( inches): ")
+if float(height)*2.54>=145:
+    print("you are tall enough to ride.")
+else:
+    print("you need to grow some more to ride.")
