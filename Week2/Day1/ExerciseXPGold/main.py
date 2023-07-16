140733ba89c1f9b217e4793432aa0f3c5f0febbd
Gabriel Dischon <gabrielgdischon@gmail.com>
2023-07-16 15:26:24 +0300

    XPNinjaweek2day1

---
+++ Week2/Day1/ExerciseXPGold/main.py
@@ -0,0 +1,14 @@
+# exercise 1
+# print("Hello world\nHello world\nHello world\nHello world\nI love python\nI love python\nI love python\nI love python\n")
+
+# exercise 2
+month=input("Please input a month( in number): ")
+month=int(month)
+if month == 3 or month == 4 or month == 5:
+    print("Spring")
+if month == 6 or month == 7 or month == 8:
+    print("Summer")
+if month == 9 or month == 10 or month == 11:
+    print("Autumn")
+if month == 12 or month == 1 or month == 2:
+    print("Winter")
