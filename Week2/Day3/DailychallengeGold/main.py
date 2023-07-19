c29bf75fa54ac8e03c6e80e05097d9dc73ed2705
Gabriel Dischon <gabrielgdischon@gmail.com>
2023-07-19 11:13:13 +0300

    week2day3dailychallengegold

--- Week2/Day3/DailychallengeGold/main.py
+++ Week2/Day3/DailychallengeGold/main.py
@@ -1,8 +1,14 @@
-birthdate = input("Gime your birthday int his format, DD/MM/YYYY pls: ").split("/")
-age = 2023 - int(birthdate[2])
-candle_numeber = int(str(age)[-1])
-if candle_numeber % 2 == 0:
-    candles = (int((10 - candle_numeber)/2) * "_") + int(candle_numeber/2) * "i" + "_" + int(candle_numeber/2) * "i" + (int((10 - candle_numeber)/2) * "_")
+text = input("Gime the text: ")
+op = input("d for decrypt and e for encrypt: ")
+if op == "d" :
+    cypher_text = ""
+    for letter in text:
+        cypher_text += chr(ord(letter) + 3)
+    print(cypher_text)
+elif op == "e" :
+    decypher_text = ""
+    for letter in text:
+        decypher_text += chr(ord(letter) - 3)
+    print(decypher_text)
 else:
-    candles = (int((11 - candle_numeber)/2) * "_") + candle_numeber * "i" + (int((11 - candle_numeber)/2) * "_")
-print(f"    {candles}\n   |:H:a:p:p:y:|\n __|___________|__\n|^^^^^^^^^^^^^^^^^|\n|:B:i:r:t:h:d:a:y:|\n|                 |\n~~~~~~~~~~~~~~~~~~~")
+    print("invalid operation")
