da6937cf98a960c6b5e81ac7878bafc5cf90a2be
Gabriel Dischon <gabrielgdischon@gmail.com>
2023-07-20 10:37:20 +0300

    week2day5dailychallenge

---
+++ Week2/Day5/Dailychallenge/main.py
@@ -0,0 +1,12 @@
+# challenge 1
+# words = input("Gime a comma separated sequence of words: ").split(",")
+# words.sort()
+# print(words)
+
+# challenge 2
+# words = input("Gime a sentence: ").split()
+# big1 = words[0]
+# for word in words:
+#     if len(word) > len(big1):
+#         big1 = word
+# print(big1)
