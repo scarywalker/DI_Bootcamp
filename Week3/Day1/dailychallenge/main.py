551b9ca5dae22ac7c355f8ae7ed005432f5cade4
Gabriel Dischon <gabrielgdischon@gmail.com>
2023-07-23 20:28:04 +0300

    fixed folders, and week3day1dailychallenge

---
+++ Week3/Day1/dailychallenge/main.py
@@ -0,0 +1,20 @@
+class Farm:
+    def __init__(self,name):
+        self.name=name
+        self.inventory = {}
+
+    def add_animal(self,animal,number=1):
+        if animal in self.inventory: self.inventory[animal] += number
+        else: self.inventory[animal] = number
+    
+    def get_info(self):
+        print(f"{self.name}'s farm\n")
+        for item in self.inventory: print(f"{item} : {self.inventory[item]}\n")
+        print("    E-I-E-I-0!")
+
+macdonald = Farm("McDonald")
+macdonald.add_animal('cow',5)
+macdonald.add_animal('sheep')
+macdonald.add_animal('sheep')
+macdonald.add_animal('goat', 12)
+print(macdonald.get_info())
