0dc4582cb498551e739aded30b30fa15ae21c983
Gabriel Dischon <gabrielgdischon@gmail.com>
2023-07-23 11:29:23 +0300

    _

---
+++ Week2/Day5/Mini-Project#2-Hangman/main.py
@@ -0,0 +1,9 @@
+import random
+
+wordslist = ['correction', 'childish', 'beach', 'python', 'assertive', 'interference', 'complete', 'share', 'credit card', 'rush', 'south']
+word = random.choice(wordslist)
+
+print(len(word)*"*")
+
+if input("Gime a letter: ") in word : 
+    pass
