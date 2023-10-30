546daa98206bf2044ab63b76edfd99531bd91ff1
Gabriel Dischon <gabrielgdischon@gmail.com>
2023-10-30 11:44:38 +0200

    w7d2 exercise1

---
+++ Week7/Day2/ExerciseXP/JS/script.js
@@ -0,0 +1,29 @@
+const marioGame = {
+    detail : "An amazing game!",
+    characters : {
+        mario : {
+          description:"Small and jumpy. Likes princesses.",
+          height: 10,
+          weight: 3,
+          speed: 12,
+        },
+        bowser : {
+          description: "Big and green, Hates princesses.",
+          height: 16,
+          weight: 6,
+          speed: 4,
+        },
+        princessPeach : {
+          description: "Beautiful princess.",
+          height: 12,
+          weight: 2,
+          speed: 2,
+        }
+    },
+  }
+
+let jsonMarioGame = JSON.stringify(marioGame, null,1);
+console.log(jsonMarioGame)
+// they all become json
+
+// dont know what "add a breakpoint" means in this context
