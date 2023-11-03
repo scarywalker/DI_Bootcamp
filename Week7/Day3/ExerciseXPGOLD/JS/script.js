fc4bcd14450ba6b6cd0089e24a429a4575162aa0
Gabriel Dischon <gabrielgdischon@gmail.com>
2023-11-03 21:12:44 +0200

    a

---
+++ Week7/Day3/ExerciseXPGOLD/JS/script.js
@@ -0,0 +1,17 @@
+// 1
+// const promise1 = Promise.resolve(3);
+
+// const promise2 = 42;
+
+// const promise3 = new Promise((resolve, reject) => {
+//   setTimeout(resolve, 3000, 'foo');
+// });
+
+// Promise.all([promise1, promise2, promise3])
+// .then((results) => {
+// console.log(results);
+// }).catch((error) => {
+// console.log(error);
+// });
+
+// 2
