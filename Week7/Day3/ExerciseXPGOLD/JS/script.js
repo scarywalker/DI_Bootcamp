457e4e96317b9634a7c5c4d00c98cab37a9d700b
Gabriel Dischon <gabrielgdischon@gmail.com>
2023-11-03 21:16:11 +0200

    w7d3xpgold

--- Week7/Day3/ExerciseXPGOLD/JS/script.js
+++ Week7/Day3/ExerciseXPGOLD/JS/script.js
@@ -15,3 +15,16 @@
 // });
 
 // 2
+function timesTwoAsync(x) {
+  return new Promise(resolve => resolve(x * 2));
+}
+
+const arr = [1, 2, 3];
+const promiseArr = arr.map(timesTwoAsync);
+
+Promise.all(promiseArr)
+  .then(result => {
+    console.log(result);
+  });
+
+// the promise all waits for all promises to be resolved, so it first multiplies the array by two and only then it logs the result which is the array * 2
