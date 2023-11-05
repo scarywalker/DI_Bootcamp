1343a69c76d4bdc57e856ad40c367b7390053f2e
Gabriel Dischon <gabrielgdischon@gmail.com>
2023-11-05 16:18:09 +0200

    w57 ended

--- Week7/Day5/Daily/JS/script.js
+++ Week7/Day5/Daily/JS/script.js
@@ -1,79 +1,79 @@
-console.log('ver 2.3 ajax');
-let xhr = new XMLHttpRequest();
+const convert = document.querySelector('#convert');
+const from = document.querySelector("#from");
+const to = document.querySelector("#to");
+const amount = document.querySelector("#amount");
+const response = document.querySelector("#response");
 
-let button = document.querySelector('#button');
-let names = document.querySelector('#name');
-let height = document.querySelector('#height');
-let gender = document.querySelector('#gender');
-let birthYear = document.querySelector('#birth-year');
-let homeWorld = document.querySelector('#home-world');
-
-function getInfo() {
-  updateWithLoading();
-  let randomNumber = Math.floor((Math.random() * 88) + 1);
-  let apiUrl = 'https://swapi.dev/api/people/' + randomNumber + '/';
-  xhr.open('GET', apiUrl);
-  xhr.responseType = 'json';
-  xhr.send();
-  xhr.onload = function () {
-    if (xhr.status != 200) {
-      updateInfoWithError();
-      console.log('there was an error');
-    } else {
-      updateInfo(xhr.response);
-    }
+const currenciesAvailable = async function () {
+  try {
+    const response = await fetch(`https://v6.exchangerate-api.com/v6/be3f6a56a61da1379d642a6a/codes`);
+    const results = await response.json();
+    return results.supported_codes;
+  } catch (error) {
+    console.error("Failed to fetch currency codes:", error);
+    return [];
   }
-  xhr.onerror = function () {
-    updateInfoWithError();
-    console.log('there was an error');
-  };
-}
+};
 
-function updateInfo(resp) {
-  let url = new URL(resp.homeworld);
-  url.protocol = 'https:'
-  xhr.open('GET', url.href);
-  xhr.responseType = 'json';
-  xhr.send();
-  xhr.onload = function () {
-    if (xhr.status != 200) {
-      console.log('there was an error 2');
-    } else {
-      resp.homeworld = xhr.response.name;
-      updateCharacterInfo(resp);
+currenciesAvailable()
+  .then(codes => {
+    codes.forEach(code => {
+      const [codeValue, codeName] = code;
+      const fromOption = document.createElement("option");
+      fromOption.value = codeValue;
+      fromOption.text = codeValue;
+      from.appendChild(fromOption);
+      const toOption = document.createElement("option");
+      toOption.value = codeValue;
+      toOption.text = codeValue;
+      to.appendChild(toOption);
+    });
+  })
+  .catch(error => {
+    console.error("Failed to fetch currency codes:", error);
+  });
+
+async function convertCurrencies(currency1, currency2, currency1Amount) {
+  try {
+    const rate = async function () {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/be3f6a56a61da1379d642a6a/pair/${currency1}/${currency2}`);
+      const results = await response.json();
+      return results.conversion_rate;
     }
+    const conversionRate = await rate();
+    const convertedAmount = currency1Amount * conversionRate;
+    return `${currency1Amount} ${currency1} = ${convertedAmount} ${currency2}`;
+  } catch (error) {
+    console.error("Currency conversion error:", error);
+    return "Currency conversion failed";
   }
-  xhr.onerror = function () {
-    console.log('there was an error 3');
-  };
-  console.log(resp.homeworld);
 }
 
-function updateCharacterInfo(resp) {
-  console.log(resp.homeworld);
-  console.log(resp.name);
-
-  names.innerText = resp.name;
-  height.innerText = `Height: ${resp.height} cm`;
-  gender.innerText = `Gender: ${resp.gender}`;
-  birthYear.innerText = `Birth Year: ${resp.birth_year}`;
-  homeWorld.innerText = `Home World: ${resp.homeworld}`;
+function appendResponse(string) {
+  response.textContent = string;
 }
 
-function updateInfoWithError() {
-  names.innerText = `Oh No! That person isn't available.`;
-  height.innerText = '';
-  gender.innerText = '';
-  birthYear.innerText = '';
-  homeWorld.innerText = '';
-}
+convert.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const fromText = from.options[from.selectedIndex].text;
+  const toText = to.options[to.selectedIndex].text;
+  const amountValue = amount.value;
+  try {
+    const converted = await convertCurrencies(fromText, toText, amountValue);
+    appendResponse(converted);
+  } catch (error) {
+    console.error("Conversion failed:", error);
+    appendResponse("Conversion failed");
+  }
+});
 
-function updateWithLoading() {
-  names.innerHTML = '<i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i> <p>Loading...</p>';
-  height.innerText = '';
-  gender.innerText = '';
-  birthYear.innerText = '';
-  homeWorld.innerText = '';
+const rate = async function () {
+  try {
+    const responsed = await fetch(`https://v6.exchangerate-api.com/v6/be3f6a56a61da1379d642a6a/pair/EUR/GBP`)
+    const results = await responsed.json()
+    return results['conversion_rate'];
+  } catch (error) {
+    console.error("Rate fetch error:", error);
+    return "Rate fetch failed";
+  }
 }
-
-button.addEventListener('click', getInfo);
