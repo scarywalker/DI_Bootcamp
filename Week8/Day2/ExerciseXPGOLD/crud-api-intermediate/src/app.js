2ccc7ab24fd7fb6fbd3c934452f2554a6cd8f629
Gabriel Dischon <gabrielgdischon@gmail.com>
2023-11-08 00:30:23 +0200

    a

--- Week8/Day2/ExerciseXPGOLD/crud-api-intermediate/src/app.js
+++ Week8/Day2/ExerciseXPGOLD/crud-api-intermediate/src/app.js
@@ -1,2 +1,32 @@
-const uuid = require("uuid");
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+let posts = [];
+
+app.get('/api/posts', (req, res) => {
+  res.json(posts);
+});
+
+app.get('/api/posts/:id', (req, res) => {
+  const postId = parseInt(req.params.id);
+  const post = posts.find((p) => p.id === postId);
+  if (!post) {
+    return res.status(404).json({ error: 'Post not found' });
+  }
+  res.json(post);
+});
+
+app.post('/api/posts', (req, res) => {
+  const newPost = {
+    id: posts.length + 1,
+    title: req.body.title,
+    body: req.body.body,
+  };
+  posts.push(newPost);
+  res.status(201).json(newPost);
+});
