88c558d725e3b0b7e3cf9af0b059c79d62204a3c
Gabriel Dischon <gabrielgdischon@gmail.com>
2023-11-21 20:01:53 +0200

    w10 d1 xp error imcomplete

---
+++ Week10/Day1/ExerciseXP/err-sim/src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./App.css";
+import ErrorBoundary from "./ErrorBoundary";
+
+class BuggyCounter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
+  }
+  handleClick = () => {
+    if (this.state.counter === 5) {
+      throw new Error("I crashed");
+    }
+    this.setState((prevState) => ({
+      counter: prevState.counter + 1,
+    }));
+  };
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>{this.state.counter}</button>
+      </div>
+    );
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+      <ErrorBoundary>
+        <BuggyCounter />
+        <BuggyCounter />
+      </ErrorBoundary>
+    </div>
+  );
+}
+
+export default App;
